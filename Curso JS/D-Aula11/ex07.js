90e227fddc31dccf32287a5ed4bfb85dc69eb9e6
Arthur <VleonelArthur@gmail.com>
2024-02-21 15:29:26 -0300

    D-Aula11

---
+++ Curso JS/D-Aula11/ex07.js
@@ -0,0 +1,7 @@
+var velocidade = 68
+var velocidadelimite = 60
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if (velocidade > velocidadelimite) {
+    console.log(`Voce ultrapassou a velocidade permitida de ${velocidadelimite}Km/h. MULTADO! `)
+}
+console.log('dirija sempre usando sinto de segurança!')
