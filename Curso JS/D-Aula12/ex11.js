fb2fee99208b684dff755288e7a1c7c5667fea1e
Arthur <VleonelArthur@gmail.com>
2024-02-26 15:44:30 -0300

    Condições

---
+++ Curso JS/D-Aula12/ex11.js
@@ -0,0 +1,11 @@
+var idade = 35
+if (idade < 16 )
+    {
+        console.log('não vota')
+    } else {
+        if (idade < 18 || idade > 65 ) {
+            console.log('voto opicional')
+        } else {
+            console.log('voto obrigatório')
+        }
+    }
