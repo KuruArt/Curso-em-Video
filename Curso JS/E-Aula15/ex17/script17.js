6d9b9cd3b19997eddc73b1ed5ea1c3654886b12a
Arthur <VleonelArthur@gmail.com>
2024-03-05 13:46:23 -0300

    update_ex17

--- Curso JS/E-Aula15/ex17/script17.js
+++ Curso JS/E-Aula15/ex17/script17.js
@@ -1,13 +1,32 @@
-var res = document.getElementById('resposta')
-var n1 = document.getElementById('inicio')
-var n2 = document.getElementById('fim')
-var n3 = document.getElementById('passo')
-var incio = Number(n1.value)
-var fim = Number(n2.value)
-var passo = Number(n3.value)
-function confirm() {
-    if (inicio < fim) {
-      res.innerHTML = `${resnum}`
-      resnum ++
-    }
+function contar() {
+  var ini = document.getElementById('txti')
+  var fim = document.getElementById('txtf')
+  var passo = document.getElementById('txtp')
+  var res = document.getElementById('resposta')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+      window.alert('[ERRO] Faltam dados!')
+      res.innerHTML = 'Impossivel contar!'
+    } else {
+      res.innerHTML = '<p> Contando ... </p>'
+      var i = Number(ini.value)
+      var f = Number(fim.value)
+      var p = Number(passo.value)
+      var c = i
+      if(p <= 0) {
+        window.alert(' Passo 0 é invalido! Contado apartir de 1!')
+        p = 1
+      }
+      if(i < f){
+        while (c < f) {
+          c += p
+          res.innerHTML += `  ${c} \u{1F449} `
+        }
+      } else {
+        while (c > f) {
+          c -= p
+          res.innerHTML += ` ${c}\u{1F449} `
+        }
+      }
+    res.innerHTML += `\u{1F3C1}`
+}
 }
